2b28111c4da27597cd3f9391c197721853ec2c62
jrycho <jrychtera@gmail.com>
2025-11-20 13:03:06 +0100

    db forcing certifi

--- app/db_files/core/database.py
+++ app/db_files/core/database.py
@@ -1,13 +1,16 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 import os
+import certifi
 
 load_dotenv()  # Load variables from .env
 
 MONGO_URI = os.getenv("MONGO_URI") #, "mongodb://localhost:27017")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "nutrition_app")
 
-client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncIOMotorClient(MONGO_URI,
+                            tlsCAFile=certifi.where(),  # <- key bit
+                            serverSelectionTimeoutMS=5000,)
 db = client[MONGO_DB_NAME]
 
 users_collection = db["users"]
