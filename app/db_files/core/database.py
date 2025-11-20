30a518695ca6a95b0c185387ed6ab4d497058781
jrycho <jrychtera@gmail.com>
2025-11-20 12:26:51 +0100

    Tryout for render.com

--- app/db_files/core/database.py
+++ app/db_files/core/database.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()  # Load variables from .env
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI") #, "mongodb://localhost:27017")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "nutrition_app")
 
 client = AsyncIOMotorClient(MONGO_URI)
