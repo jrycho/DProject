a0e2a2c275bc5e746ee6f61fb7f0e2b58c14b8c7
jrycho <jrychtera@gmail.com>
2025-11-25 14:49:42 +0100

    FIXED: Typo

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://dproject-frontend.onrender.com/"
+    "https://dproject-frontend.onrender.com"
 ]
 
 app.add_middleware(
